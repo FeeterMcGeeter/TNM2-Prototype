026cf6373b511cb9923f88695f6164bb19339064
Jeffrey Davis <davisj0214@yahoo.com>
2023-03-18 01:12:00 -0500

    added responsive menu to navbar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // GIVING THE CURRENT LINK THE "ACTIVE" TAG FOR STYLING
 function setActive() {
-    navLink = document.getElementById("navbar").getElementsByClassName("nav-link");
+    navLink = document.getElementById("nav").getElementsByClassName("nav__list-link");
     for (i = 0; i < navLink.length; i++) {
         if (document.location.href.indexOf(navLink[i].href) >= 0) {
             navLink[i].className = "active";
@@ -22,7 +22,20 @@ function alertUser() {
     alert("Submission Successful");
 }
 
-// NEED TO FIX THE IMAGES ON THE DOG, CAT, AND FISH PAGE
+// HAMBURGER MENU FOR RESPONSIVE NAVBAR
+const burgerMenu = document.querySelector(".nav__hamburger");
+const navList = document.querySelector("#nav__list");
+
+burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("current");
+    navList.classList.toggle("current");
+  });
+  
+// CLOSES THE MOBILE NAVBAR WHEN CLICKING ON LINKS
+document.querySelectorAll(".nav__list-link").forEach(n => n.addEventListener("click", () => {
+burgerMenu.classList.remove("current");
+navList.classList.remove("current");
+}))
+
 // NEED TO MAKE THE PAGES MORE RESPONSIVE 
-// FINISH STYLING THE FORM
 // TRY TO ADD FUNCTIONALITY TO THE SEARCH BAR
